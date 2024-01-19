5c20d1da0ff707af6b9847ea7f1985f4d8805fe8
Iskhak <madismadanbekov@gmail.com>
2024-01-19 19:51:36 +0600

    test commit

--- src/classwork1/Main.java
+++ src/classwork1/Main.java
@@ -4,19 +4,13 @@ import classwork1.model.Catalog;
 import classwork1.model.Product;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Product product1 = new Product("Шампунь Жумайсыбма", 2.69, "SHJ-69");
-        Product product2 = new Product("Дилдо 23сантиметра", 224.69, "D-23");
-        Product product3 = new Product("Latex glove ultra pro max", 1213.69, "LX-99");
-        Product product4 = new Product("Горелый еврей", 0.69, "GI-41");
-        Product product5 = new Product("плов узбекский", 1500.69, "UZ-998");
-        Product product6 = new Product("Гель-скраб лля мозга (маленбкий пенис)", 13.5, "ISH-13");
-
-        List<Product> products = new ArrayList<>(List.of(product1, product2, product3, product4, product5, product6));
+        List<Product> products = getProducts();
         Catalog catalog = new Catalog(products);
 
         System.out.println("------------Catalog------------");
@@ -30,6 +24,7 @@ public class Main {
             System.out.println("1. Создать новый продукт");
             System.out.println("2. Удалить сущ продукт");
             System.out.println("3. Вытщаить каталог");
+            System.out.println("7. Найти макс значение");
             int operation = scanner.nextInt();
 
             if (operation == 1){
@@ -54,11 +49,38 @@ public class Main {
                 allProducts.removeIf(p -> p.getArticle().equals(article));
                 catalog.setProducts(allProducts);
             }
+            if (operation == 7) {
+                if (!catalog.getProducts().isEmpty()) {
+                    Product mostExpensiveProduct = catalog.getProducts().stream()
+                            .max(Comparator.comparingDouble(Product::getPrice))
+                            .orElse(null);
+
+                    if (mostExpensiveProduct != null) {
+                        System.out.println("Самый дорогой товар:\n" + mostExpensiveProduct);
+                    } else {
+                        System.out.println("Нет доступных товаров.");
+                    }
+                } else {
+                    System.out.println("Нет доступных товаров.");
+                }
+            }
 
             if (operation == 3){
                 catalog.getProducts().forEach(System.out::println);
             }
         }
 
+    }
+
+    private static List<Product> getProducts() {
+        Product product1 = new Product("Шампунь Жумайсыбма", 2.69, "SHJ-69");
+        Product product2 = new Product("Дилдо 23сантиметра", 224.69, "D-23");
+        Product product3 = new Product("Latex glove ultra pro max", 1213.69, "LX-99");
+        Product product4 = new Product("Горелый еврей", 0.69, "GI-41");
+        Product product5 = new Product("плов узбекский", 1500.69, "UZ-998");
+        Product product6 = new Product("Гель-скраб лля мозга (маленбкий пенис)", 13.5, "ISH-13");
+
+        List<Product> products = new ArrayList<>(List.of(product1, product2, product3, product4, product5, product6));
+        return products;
     }
 }
